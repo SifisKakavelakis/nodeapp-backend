df84bc66a2a5408cd8b052072f289030822ea61a
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-05-06 18:57:45 +0300

    feat : add google login

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import * as authService from '../services/auth.service';
+import { resourceLimits } from 'node:worker_threads';
 
 export const login = async(req:Request, res:Response, next: NextFunction) => {
   try{
@@ -7,6 +8,18 @@ export const login = async(req:Request, res:Response, next: NextFunction) => {
     const result = await authService.login(username, password);
     if (!result) return res.status(401).json({message: "Invalid credentials"});
     res.status(200).json({token: result.token, user:{username:result.user.username} })
+  } catch (err) {
+    next (err);
+  }
+}
+
+export const googleLogin = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {token} = req.body;
+    const result = await authService.googleLogin(token);
+    if(!result) 
+      return res.status(401).json({status: false, message: result});
+    res.status(200).json({status: true, token: result.token});
   } catch (err) {
     next (err);
   }
